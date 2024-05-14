2ec6ca94de330bec1c63f35934c017235bcb9d38
Akshay Vr <avr02339@gmail.com>
2024-05-14 18:41:38 +0530

    Delete from cart route created

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -38,4 +38,34 @@ const addToCart = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = { addToCart }
+const removeFromCart = asyncHandler(async (req, res) => {
+    const userId = req.user._id; // Assuming req.user._id is the user's ID
+    console.log(userId);
+    const { name } = req.body;
+
+    // Find the cart belonging to the user
+    const cartExist = await Cart.findOne({ userId: userId });
+
+    if (cartExist) {
+        // Find the product in the cart's products array
+        const productIndex = cartExist.products.findIndex(item => item.name === name);
+
+        if (productIndex !== -1) {
+            // If product exists in the cart, remove it from the array
+            cartExist.products.splice(productIndex, 1);
+            
+            // Save the updated cart
+            await cartExist.save();
+            
+            // Send the updated cart as response
+            res.send(cartExist);
+        } else {
+            res.status(404).send("Product not found in the cart");
+        }
+    } else {
+        res.status(404).send("Cart not found");
+    }
+});
+
+
+module.exports = { addToCart,removeFromCart }
