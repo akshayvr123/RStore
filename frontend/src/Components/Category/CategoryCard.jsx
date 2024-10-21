ba431d3224a8f328d919bfe6d32ac5fe90ae97b7
Akshay Vr <avr02339@gmail.com>
2024-10-21 18:44:11 +0530

    Build a loader for category loading

--- frontend/src/Components/Category/CategoryCard.jsx
+++ frontend/src/Components/Category/CategoryCard.jsx
@@ -2,17 +2,18 @@ import React, { useEffect, useState } from 'react';
 import './Category.css';
 import useCategoryNames from '../../CustomHooks/useCategoryNames';
 import { useNavigate } from 'react-router-dom';
+import { ColorRing } from 'react-loader-spinner'
 
 const CategoryCard = () => {
-  const [category,setCategoryDetails]=useState([])
-  const BASE_URL=import.meta.env.VITE_BACKEND_BASE_URL
+  const [category, setCategoryDetails] = useState([])
+  const BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL
   const [categoryNames] = useCategoryNames(`${BASE_URL}/api/product/categorynames`);
-  const navigate=useNavigate()
-  useEffect(()=>{
+  const navigate = useNavigate()
+  useEffect(() => {
     setCategoryDetails(categoryNames?.categoryNames);
-  },[categoryNames])
+  }, [categoryNames])
 
-  const handleClick=(cat)=>{
+  const handleClick = (cat) => {
     console.log(cat);
     navigate(`/product/${cat.name}`)
   }
@@ -22,9 +23,9 @@ const CategoryCard = () => {
       <h2 className='scrollbar-hide text-2xl sm:text-3xl pt-7 pl-[2rem] sm:pl-[3rem] md:pl-[7rem] font-bold'>Category</h2>
       <div className='item-container '>
 
-        {category?.map((cat,index) => {
+        {category ? category.map((cat, index) => {
           return (
-            <div onClick={()=>handleClick(cat)} key={index} className='item w-80 sm:w-[428px] '>
+            <div onClick={() => handleClick(cat)} key={index} className='item w-80 sm:w-[428px] '>
               <img className=' w-full h-[210px] rounded-b-sm' src={cat.imageUrl} alt='' />
               <div className='content'>
                 <p className='normal-txt pl-[.60rem]'>{cat.name}</p>
@@ -32,7 +33,21 @@ const CategoryCard = () => {
               </div>
             </div>
           )
-        })}
+        })
+          :
+          <div className='w-80 sm:w-[428px]  text-center flex items-center justify-center'>
+            {/* Loader */}
+            <ColorRing
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="color-ring-loading"
+              wrapperStyle={{}}
+              wrapperClass="color-ring-wrapper"
+              colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+            />
+          </div>
+        }
 
       </div>
     </div>
